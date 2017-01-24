1c04ee74693d5a26bc22d8e4f1da1262e26e7e2d
kmmx <kmmx@hsoft.local>
2017-01-23 20:43:16 -0600

    sesion 3

--- sesion_3/prog.4.py
+++ sesion_3/prog.4.py
@@ -1,8 +1,17 @@
 from openpyxl import load_workbook
 from geg import *
 
-wb = load_workbook("data.xlsx")
+wb = load_workbook("puntos.xlsx")
 
 ws = wb.active
 
-print automatic_load_data(ws, "B20")
+puntos = automatic_load_data(ws, "A2")
+
+def f(x, y):
+    return x**2 + y**2
+
+for p in puntos:
+    x = p["X"]
+    y = p["Y"]
+    z = f(x, y)
+    print "%f, %f, %f" %(x, y, z)
